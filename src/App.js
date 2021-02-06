1302eaf4eeafb81ef202e82280db6ab02301d345
Greg Stula <gregstula@me.com>
2021-02-05 19:45:12 -0500

    Create initial skeleton

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from '@material-ui/core/Button';
+import Header from './components/Header';
+import Container from '@material-ui/core/Container';
+import CssBaseline from '@material-ui/core/CssBaseline';
+
+const sections = [
+    {title:'About', url: '#'},
+    {title: 'Resume', url: '#'},
+    {title: 'Projects', url: '#'},
+    {title: 'LinkedIn', url: '#'},
+    {title: 'GitHub', url: '#'},
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+        <CssBaseline />
+        <Container maxWidth="lg">
+            <Header title={"Greg Stula"} sections={sections} />
+        </Container>
+      </>
   );
 }
 
